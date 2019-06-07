0614d073e91115797d398d3ae7e805a49df9387d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-06-07 23:28:29 +0200

    Add a test with an invalid .taskcluster.yml file
    From an error which happened in the real world.
    
    Fixes #5

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -4,11 +4,25 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import os
+import re
+
+import jsone
+import pytest
 
 from taskcluster_yml_validator import validate
 
 FIXTURES_DIR = os.path.join(os.path.dirname(__file__), "fixtures")
 
 
-def test_get_qaneeded_labels():
+def test_valid_taskcluster_yml():
     validate(os.path.join(FIXTURES_DIR, "bugbug.taskcluster.yml"))
+
+
+def test_invalid_taskcluster_yml():
+    with pytest.raises(
+        jsone.shared.InterpreterError,
+        match=re.escape(
+            "InterpreterError at template.tasks[8].payload.command[2]: unknown context value tag"
+        ),
+    ):
+        validate(os.path.join(FIXTURES_DIR, "bugbug_invalid.taskcluster.yml"))
